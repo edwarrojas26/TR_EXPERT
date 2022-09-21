65c7191781c2dd3bfb2ab5a8b2bc2855c13506c0
Your Nameedwarrojas26 <edrojas843@misena.edu.co>
2022-09-21 11:33:03 -0500

    Base de datos 6

--- script_trexpert_vs1.sql
+++ script_trexpert_vs1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-09-2022 a las 16:21:44
+-- Tiempo de generación: 11-09-2022 a las 05:42:39
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
