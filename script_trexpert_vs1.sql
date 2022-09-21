dd4d92fc5d7c0ac4d230f07de8d7132c6e1ccf55
Your Nameedwarrojas26 <edrojas843@misena.edu.co>
2022-09-21 11:43:01 -0500

    Base de datos 7

--- script_trexpert_vs1.sql
+++ script_trexpert_vs1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-09-2022 a las 05:42:39
+-- Tiempo de generación: 12-09-2022 a las 18:34:46
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -79,20 +79,23 @@ DELIMITER ;
 
 CREATE TABLE `cliente` (
   `idCliente` bigint(20) NOT NULL,
-  `idUsuario` bigint(20) NOT NULL
+  `idUsuarioFK` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `ejercicio`
+-- Estructura de tabla para la tabla `ejercicios`
 --
 
-CREATE TABLE `ejercicio` (
+CREATE TABLE `ejercicios` (
   `idEjercicio` bigint(20) NOT NULL,
-  `idTrabajo` bigint(20) DEFAULT NULL,
   `nombreEjercicio` varchar(50) DEFAULT NULL,
-  `descripcionEjercicio` varchar(100) DEFAULT NULL
+  `descripcionEjercicio` varchar(100) NOT NULL,
+  `series` int(11) NOT NULL,
+  `repeticiones` int(11) NOT NULL,
+  `descanso` float NOT NULL,
+  `peso` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -103,7 +106,7 @@ CREATE TABLE `ejercicio` (
 
 CREATE TABLE `entrenador` (
   `idEntrenador` bigint(20) NOT NULL,
-  `idUsuario` bigint(20) DEFAULT NULL,
+  `idUsuarioFK` bigint(20) NOT NULL,
   `area` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -115,10 +118,9 @@ CREATE TABLE `entrenador` (
 
 CREATE TABLE `medida` (
   `idMedida` bigint(20) NOT NULL,
-  `nombreMedida` varchar(50) DEFAULT NULL,
-  `valor` float DEFAULT NULL,
-  `descripcion` varchar(100) DEFAULT NULL,
-  `tipoMedida` varchar(50) DEFAULT NULL
+  `CodigoFK` bigint(20) DEFAULT NULL,
+  `valorI` float DEFAULT NULL,
+  `valorF` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -128,25 +130,24 @@ CREATE TABLE `medida` (
 --
 
 CREATE TABLE `plan_entrenamiento` (
-  `idCliente` bigint(20) NOT NULL,
-  `idTrabajo` bigint(20) DEFAULT NULL,
-  `idMedida` bigint(20) DEFAULT NULL,
+  `idClienteFk` bigint(20) DEFAULT NULL,
+  `idMedidaFK` bigint(20) NOT NULL,
+  `idEjercicioFK` bigint(20) DEFAULT NULL,
   `idEntrenador` bigint(20) DEFAULT NULL,
-  `infoPlan` varchar(100) DEFAULT NULL
+  `oservaciones` varchar(100) DEFAULT NULL,
+  `fechaCreacion` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `trabajo`
+-- Estructura de tabla para la tabla `tipomedida`
 --
 
-CREATE TABLE `trabajo` (
-  `idTrabajo` bigint(20) NOT NULL,
-  `series` int(11) DEFAULT NULL,
-  `repeticiones` int(11) DEFAULT NULL,
-  `descanso` float DEFAULT NULL,
-  `peso` float DEFAULT NULL
+CREATE TABLE `tipomedida` (
+  `Codigo` bigint(20) NOT NULL,
+  `nombreParte` varchar(50) NOT NULL,
+  `categoria` enum('Tren superior','Tren Inferior','saludAlimentacion','factorRiesgo') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -181,35 +182,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idUsuario`, `numDoc`, `tipoDoc`, `nombre`, `apellido`, `fechaNacimiento`, `edad`, `direccion`, `telefono`, `correo`, `Tipo_sangre`, `EPS`, `alergias`, `estado`, `sexo`, `rol`, `contraseña`, `fechaIngreso`) VALUES
-(8, 1025140348, 'TI', 'Edwar ', 'Rojas', '2022-08-29', 18, 'Diagonal 59 sur #3B-72', 3227123006, 'edwarrojas2003@gmail.com', 'AB+', 'Sura', 'Ninguna', 'Inactivo', 'm', 'Cliente', 'Cliente', '2022-09-08 00:48:25'),
-(9, 39765813, 'TI', 'Cristian', 'Avila', '2003-12-18', 18, 'Diagonal 59 sur #3B-72', 48138184, 'edwarrojas@gmail.com', 'A+', 'Sura', 'Ninguna', 'Inactivo', 'M', 'Cliente', 'edwar', '2022-09-08 03:46:48'),
-(10, 9632148, 'CC', 'Myke', 'Towers', '2003-12-15', 19, 'Diagonal 60 sur #3B-72', 74123698, 'edwarrojas20008@gmail.com', 'AB+', 'Famisanar', 'Todas', 'Activo', 'F', 'Cliente', 'angelica', '2022-09-08 03:49:53');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `valor`
---
-
-CREATE TABLE `valor` (
-  `idValor` bigint(20) NOT NULL,
-  `idMedida` bigint(20) DEFAULT NULL,
-  `idCliente` bigint(20) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `valor` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `valoracionmedidascliente`
---
-
-CREATE TABLE `valoracionmedidascliente` (
-  `idValoracion` bigint(20) NOT NULL,
-  `idMedida` bigint(20) DEFAULT NULL,
-  `observacion` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(1, 1025140348, 'CC', 'Edwar', 'Rojas', '2003-12-18', 18, 'Diagonal 59 sur #3B-72', 3235647223, 'edwarrojas2003@gmail.com', 'A+', 'Sánitas', 'Ninguna', 'Activo', 'M', 'Cliente', 'edwar', '2022-09-12 06:37:56'),
+(2, 39765813, 'CC', 'Ana', 'Vargas', '1955-10-29', 66, 'Diagonal 59 sur #3B-72', 3178144300, 'edwarrojas@gmail.com', 'A-', 'Famisanar', 'Todas', 'Inactivo', 'M', 'Cliente', 'asdfghjklÃ±', '2022-09-12 06:51:01'),
+(3, 19348706, 'TI', 'Mike', 'Galindo', '1995-03-15', 28, 'Diagonal 59 sur #3B-72', 3227123006, 'edwar@gmail.com', 'A-', 'Convida', 'Ninguna', 'Activo', 'M', 'Cliente', 'dfghjklÃ±', '2022-09-12 07:03:43');
 
 --
 -- Índices para tablas volcadas
@@ -219,23 +194,20 @@ CREATE TABLE `valoracionmedidascliente` (
 -- Indices de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`idCliente`),
-  ADD KEY `idUsuario` (`idUsuario`);
+  ADD PRIMARY KEY (`idCliente`);
 
 --
--- Indices de la tabla `ejercicio`
+-- Indices de la tabla `ejercicios`
 --
-ALTER TABLE `ejercicio`
+ALTER TABLE `ejercicios`
   ADD PRIMARY KEY (`idEjercicio`),
-  ADD UNIQUE KEY `nombreEjercicio` (`nombreEjercicio`),
-  ADD KEY `idTrabajo` (`idTrabajo`);
+  ADD UNIQUE KEY `nombreEjercicio` (`nombreEjercicio`);
 
 --
 -- Indices de la tabla `entrenador`
 --
 ALTER TABLE `entrenador`
-  ADD PRIMARY KEY (`idEntrenador`),
-  ADD KEY `idUsuario` (`idUsuario`);
+  ADD PRIMARY KEY (`idEntrenador`);
 
 --
 -- Indices de la tabla `medida`
@@ -247,16 +219,13 @@ ALTER TABLE `medida`
 -- Indices de la tabla `plan_entrenamiento`
 --
 ALTER TABLE `plan_entrenamiento`
-  ADD PRIMARY KEY (`idCliente`),
-  ADD KEY `idTrabajo` (`idTrabajo`),
-  ADD KEY `idMedida` (`idMedida`),
-  ADD KEY `idEntrenador` (`idEntrenador`);
+  ADD PRIMARY KEY (`idMedidaFK`);
 
 --
--- Indices de la tabla `trabajo`
+-- Indices de la tabla `tipomedida`
 --
-ALTER TABLE `trabajo`
-  ADD PRIMARY KEY (`idTrabajo`);
+ALTER TABLE `tipomedida`
+  ADD PRIMARY KEY (`Codigo`);
 
 --
 -- Indices de la tabla `usuario`
@@ -265,21 +234,6 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idUsuario`),
   ADD UNIQUE KEY `numDoc` (`numDoc`),
   ADD UNIQUE KEY `correo` (`correo`);
-
---
--- Indices de la tabla `valor`
---
-ALTER TABLE `valor`
-  ADD PRIMARY KEY (`idValor`),
-  ADD KEY `idMedida` (`idMedida`),
-  ADD KEY `idCliente` (`idCliente`);
-
---
--- Indices de la tabla `valoracionmedidascliente`
---
-ALTER TABLE `valoracionmedidascliente`
-  ADD PRIMARY KEY (`idValoracion`),
-  ADD KEY `idMedida` (`idMedida`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -292,9 +246,9 @@ ALTER TABLE `cliente`
   MODIFY `idCliente` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `ejercicio`
+-- AUTO_INCREMENT de la tabla `ejercicios`
 --
-ALTER TABLE `ejercicio`
+ALTER TABLE `ejercicios`
   MODIFY `idEjercicio` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
@@ -310,72 +264,10 @@ ALTER TABLE `medida`
   MODIFY `idMedida` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `trabajo`
---
-ALTER TABLE `trabajo`
-  MODIFY `idTrabajo` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT de la tabla `valor`
---
-ALTER TABLE `valor`
-  MODIFY `idValor` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `valoracionmedidascliente`
---
-ALTER TABLE `valoracionmedidascliente`
-  MODIFY `idValoracion` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
-
---
--- Filtros para la tabla `ejercicio`
---
-ALTER TABLE `ejercicio`
-  ADD CONSTRAINT `ejercicio_ibfk_1` FOREIGN KEY (`idTrabajo`) REFERENCES `trabajo` (`idTrabajo`);
-
---
--- Filtros para la tabla `entrenador`
---
-ALTER TABLE `entrenador`
-  ADD CONSTRAINT `entrenador_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
-
---
--- Filtros para la tabla `plan_entrenamiento`
---
-ALTER TABLE `plan_entrenamiento`
-  ADD CONSTRAINT `plan_entrenamiento_ibfk_1` FOREIGN KEY (`idTrabajo`) REFERENCES `trabajo` (`idTrabajo`),
-  ADD CONSTRAINT `plan_entrenamiento_ibfk_2` FOREIGN KEY (`idMedida`) REFERENCES `medida` (`idMedida`),
-  ADD CONSTRAINT `plan_entrenamiento_ibfk_3` FOREIGN KEY (`idEntrenador`) REFERENCES `entrenador` (`idEntrenador`),
-  ADD CONSTRAINT `plan_entrenamiento_ibfk_4` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`);
-
---
--- Filtros para la tabla `valor`
---
-ALTER TABLE `valor`
-  ADD CONSTRAINT `valor_ibfk_1` FOREIGN KEY (`idMedida`) REFERENCES `medida` (`idMedida`),
-  ADD CONSTRAINT `valor_ibfk_2` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`);
-
---
--- Filtros para la tabla `valoracionmedidascliente`
---
-ALTER TABLE `valoracionmedidascliente`
-  ADD CONSTRAINT `valoracionmedidascliente_ibfk_1` FOREIGN KEY (`idMedida`) REFERENCES `medida` (`idMedida`);
+  MODIFY `idUsuario` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
